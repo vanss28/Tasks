e28de506005c7f2a9fc6a8b55ff6af433d4e0731
vanss28 <136988144+vanss28@users.noreply.github.com>
2023-09-26 22:22:02 +0530

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -1,9 +1,7 @@
 function Occurrences(words) {
-  //empty object initalize karna to store word counts
   const wordCounts = {};
   //looping thru each word in the array
   for (let word of words) {
-    //same property matlab word exists already toh increment the same property as one
     if (wordCounts.hasOwnProperty(word)) {
       wordCounts[word] += 1;
       //create a new property initialising the new word
@@ -11,21 +9,21 @@ function Occurrences(words) {
       wordCounts[word] = 1;
     }
   }
-  //convrting object into array jaha words=key and counts=values
+  
   const sortedWordCounts = Object.entries(wordCounts).sort(
    function Compare(a,b){
     return b[1] - a[1];
   }
       );
   for (const [word, count] of sortedWordCounts) {
-    //word kitni baar occured hai
+  
     console.log(`${word}: ${count}`);
   }
   
  
   return Object.values(wordCounts);
 }
-//array of words to be counted defined idhar
+
 const words = ["green", "red", "yellow", "red"];
 //calling the function Occureces with the word arrays
 let count = Occurrences(words);
